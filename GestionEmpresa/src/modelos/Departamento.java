00eeb0f3fec96eb9f202d409f1e22e492238ceec
manulopez14 <mlopez14@ieszaidinvergeles.org>
2023-01-12 14:09:13 +0100

    Created two classes Departamento, Empleado

--- GestionEmpresa/src/modelos/Departamento.java
+++ GestionEmpresa/src/modelos/Departamento.java
@@ -9,5 +9,34 @@ package modelos;
  * @author Manuel Lopez Rojas
  */
 public class Departamento {
+    int idDepartamento;
+    String nombre;
+
+    public Departamento(int idDepartamento, String nombre) {
+        this.idDepartamento = idDepartamento;
+        this.nombre = nombre;
+    }
+
+    public int getIdDepartamento() {
+        return idDepartamento;
+    }
+
+    public void setIdDepartamento(int idDepartamento) {
+        this.idDepartamento = idDepartamento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Departamento{" + "idDepartamento=" + idDepartamento + ", nombre=" + nombre + '}';
+    }
+    
     
 }
---
+++ GestionEmpresa/src/modelos/Empleado.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos;
+
+/**
+ *
+ * @author Manuel Lopez Rojas
+ */
+public class Empleado {
+    // Atributos clase
+    int idEmpleado;
+    String nombre, apellidos, email;
+    Departamento dpto;
+    float salario;
+    
+    // Constructores
+    public Empleado(int idEmpleado, String nombre, String apellidos, String email, Departamento dpto, float salario) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.dpto = dpto;
+        this.salario = salario;
+    }
+    
+    public Empleado() {
+        this.idEmpleado = -1;
+        this.nombre = null;
+        this.apellidos = null;
+        this.email = null;
+        this.dpto = null;
+        this.salario = 0;
+    }
+    
+    // Metodos SET
+
+    public void setIdEmpleado(int idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDpto(Departamento dpto) {
+        this.dpto = dpto;
+    }
+
+    public void setSalario(float salario) {
+        this.salario = salario;
+    }
+    
+    // Metodos GET
+
+    public int getIdEmpleado() {
+        return idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Departamento getDpto() {
+        return dpto;
+    }
+
+    public float getSalario() {
+        return salario;
+    }
+
+    
+    
+    
+    
+    
+    
+}
